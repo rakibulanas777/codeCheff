4aaf40ea8a1dcd60c3a16817c9bd39c4cc0a31e8
unknown <rakibulanas033@gmail.com>
2022-10-27 22:20:42 +0600

    solve Ezio and Guards problem

---
+++ Ezio.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    int X,Y;
+   
+    for(int i=0;i<T;i++){
+        cin>>X>>Y;
+        if(X>=Y)  cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;       
+    }
+}
