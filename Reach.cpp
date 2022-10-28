4ac21fe0965474af431cf71e0eefab5f3aafa966
unknown <rakibulanas033@gmail.com>
2022-10-28 10:08:12 +0600

    solve reach on time problem

---
+++ Reach.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    int X;
+   
+    for(int i=0;i<T;i++){
+        cin>>X;
+        if(X>=30) cout<<"YES"<<endl;   
+        else cout<<"NO"<<endl;   
+    }
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+for(int i=0;i<T;i++){
+        cin>>X;
+        if(X>=30) cout<<"YES"<<endl;   
+        else cout<<"NO"<<endl;   
+    }
