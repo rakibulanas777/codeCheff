1ae3fd38e17f7f90a680076b29743cc2fc9b7027
unknown <rakibulanas033@gmail.com>
2022-11-02 22:12:42 +0600

    solve problem

---
+++ Office.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    int X,Y;
+   
+    for(int i=0; i<T; i++){
+        cin>>X>>Y;
+        cout<<(X+X+X+X)+Y<<endl;
+    }
+}
