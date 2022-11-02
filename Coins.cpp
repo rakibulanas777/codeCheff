7a4e044a0f56203d43d571855581e9b91e2aac1b
unknown <rakibulanas033@gmail.com>
2022-11-02 21:48:23 +0600

    solve coins

---
+++ Coins.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    int X,Y;
+   
+    for(int i=0; i<T; i++){
+        cin>>X>>Y;
+        cout<<X*10+Y*5<<endl;
+    }
+}
