d19e9bd77d4ef665bc9e48c55f541011d11c125e
unknown <rakibulanas033@gmail.com>
2022-11-02 21:27:19 +0600

    solve shopping change

---
+++ Shopping.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    int X;
+   
+    for(int i=0; i<T; i++){
+        cin>>X;
+        cout<<100-X<<endl; 
+    }
+}
